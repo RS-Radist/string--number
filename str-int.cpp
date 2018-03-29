38dae8f38c58734888eb4fe560fb00506c2ce13f
Rodion Loskutov <rodionloskutov@iMac-Rodion.local>
2018-03-29 22:47:26 +0300

    new

--- str-int.cpp
+++ str-int.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 float sum;
-float leftOper,rightOper;
-char oper;
+float leftOper,rightOper,rightOper2;
+char oper_znak1,oper_znak2;
 
 float setString(string temp)
 {
@@ -15,45 +15,108 @@ float setString(string temp)
     }
     for (int i=0;i<temp.size();++i)
     {
-        if (temp[i]!=43)
+        
+        if (arrInt[i]!=43||arrInt[i]!=45||arrInt[i]!=42||arrInt[i]!=47||arrInt[i]!=32)
         {
             leftOper=atoi(temp.c_str());
         }
     }
     for (int i=0;i<temp.size();++i)
     {
-        if (arrInt[i]==43||arrInt[i]==45||arrInt[i]==42)
+        if (arrInt[i]==43||arrInt[i]==45||arrInt[i]==42||arrInt[i]==47)
         {
-            oper=(char)arrInt[i];
+            if (arrInt[i]==32)
+            {
+                while (arrInt[i]==43||arrInt[i]==45||arrInt[i]==42||arrInt[i]==47)
+                oper_znak1=(char)arrInt[i];
+            }
+            oper_znak1=(char)arrInt[i];
             int tepN=i+1;
+            if (arrInt[tepN])
             for(int j=tepN;j<temp.size();++j)
             {
                 rightOper=atoi(temp.c_str()+tepN);
+                
+                if (arrInt[i]==43||arrInt[i]==45||arrInt[i]==42||arrInt[i]==47)
+                {
+                    oper_znak2=(char)arrInt[i];
+                    int tepN=i+1;
+                    rightOper2=atoi(temp.c_str()+tepN);
+                }
             }
         }
     }
     if (!rightOper)
     {
         sum=leftOper;
-        return leftOper;
     }
-    else
+    if(rightOper)
     {
-        if (oper=='+')
+        if (oper_znak1=='+')
             sum=leftOper+rightOper;
-        if (oper=='-')
+        if (oper_znak1=='-')
             sum=leftOper-rightOper;
-        if (oper=='/')
+        if (oper_znak1=='/')
             sum=leftOper/rightOper;
-        if (oper=='*')
+        if (oper_znak1=='*')
             sum=leftOper*rightOper;
-        return sum;
-       
     }
+//    if (rightOper2!=0&&rightOper!=0&&leftOper!=0)
+//    {
+//        if (oper_znak1=='+'||oper_znak2=='+') // +
+//            sum=leftOper+rightOper+rightOper2;
+//        if (oper_znak1=='-'||oper_znak2=='-') // -
+//            sum=leftOper-rightOper-rightOper2;
+//        if (oper_znak1=='/'||oper_znak2=='/') // /
+//            sum=leftOper/rightOper/rightOper2;
+//        if (oper_znak1=='*'||oper_znak2=='*') // *
+//            sum=leftOper*rightOper*rightOper2;
+//
+//        if (oper_znak1=='+'||oper_znak2=='-')
+//            sum=leftOper+rightOper-rightOper2;
+//        if (oper_znak1=='+'||oper_znak2=='*')
+//            sum=leftOper+rightOper*rightOper2;    // +
+//        if (oper_znak1=='+'||oper_znak2=='/')
+//            sum=leftOper+rightOper/rightOper2;
+//
+//        if (oper_znak1=='-'||oper_znak2=='+')
+//            sum=leftOper-rightOper+rightOper2;
+//        if (oper_znak1=='-'||oper_znak2=='*')
+//            sum=leftOper-rightOper*rightOper2;    // -
+//        if (oper_znak1=='-'||oper_znak2=='/')
+//            sum=leftOper-rightOper/rightOper2;
+//
+//        if (oper_znak1=='*'||oper_znak2=='+')
+//            sum=leftOper*rightOper+rightOper2;
+//        if (oper_znak1=='*'||oper_znak2=='-')
+//            sum=leftOper*rightOper-rightOper2;    // *
+//        if (oper_znak1=='*'||oper_znak2=='/')
+//            sum=leftOper*rightOper/rightOper2;
+//
+//
+//        if (oper_znak1=='/'||oper_znak2=='+')
+//            sum=leftOper/rightOper+rightOper2;
+//        if (oper_znak1=='/'||oper_znak2=='-')
+//            sum=leftOper/rightOper-rightOper2;    // /
+//        if (oper_znak1=='/'||oper_znak2=='*')
+//            sum=leftOper/rightOper*rightOper2;
+//    }
+//    if (sum>2147483647)
+//    {
+//        cout<<"qq";
+//        __int64_t _sumUI;
+//        _sumUI=static_cast<__int64_t>(sum);
+//        return _sumUI;
+//    }
     return sum;
 }
 
 int main()
 {
-    cout<<setString("10050-50");
+    
+        cout<<setString("1000 + 44");
+    
+   
+    
+
 }
